093557ae6b28c3b8e708561f87c8f26586d06fa8
Gustavo <gus@MacBook-Pro-de-Gustavo.local>
2019-07-26 22:24:32 -0500

    Se agrega chain

---
+++ TallerIntegradorPatrones/src/Chain/Manejador.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chain;
+
+/**
+ *
+ * @author gus
+ */
+public interface Manejador {
+    public Manejador(int n,double denominacion);
+    public void setNext(Manejador);
+    boolean retirar();
+    depositar
+            }
